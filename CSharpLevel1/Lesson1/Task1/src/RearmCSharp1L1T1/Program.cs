7f3eda68c7b105cd2e657c13b5b9f62e602409b1
kanadeiar <kanadeiar@gmail.com>
2025-01-26 12:45:23 +0300

    Пересмотрение тестов и описания

--- CSharpLevel1/Lesson1/Task1/src/RearmCSharp1L1T1/Program.cs
+++ CSharpLevel1/Lesson1/Task1/src/RearmCSharp1L1T1/Program.cs
@@ -1,6 +1,6 @@
 ﻿using RearmCSharp1L1T1.Questionnaire.PresentationLayer;
 
-ConsoleHelper.PrintHeader( "Задача № 1. Написать программу «Анкета».", "RearmToDDD.NET C# Уровень 1 Лекция 1");
+ConsoleHelper.PrintHeader( "Задача № 1. Написать программу «Анкета».", "RearmToDDD.NET, C# Уровень 1, Лекция 1.");
 
 var service = new QuestionnaireService();
 
