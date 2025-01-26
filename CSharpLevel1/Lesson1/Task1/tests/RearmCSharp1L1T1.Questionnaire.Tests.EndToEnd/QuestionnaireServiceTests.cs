38600d436b592de40d3e76498e1a9c114af525ca
kanadeiar <kanadeiar@gmail.com>
2025-01-26 12:20:18 +0300

    Доработки в части безопасности #5

--- CSharpLevel1/Lesson1/Task1/tests/RearmCSharp1L1T1.Questionnaire.Tests.EndToEnd/QuestionnaireServiceTests.cs
+++ CSharpLevel1/Lesson1/Task1/tests/RearmCSharp1L1T1.Questionnaire.Tests.EndToEnd/QuestionnaireServiceTests.cs
@@ -19,7 +19,8 @@ public class QuestionnaireServiceTests
         ConsoleHelper.console = mock.Object;
         var sut = new QuestionnaireService();
 
-        var actual = sut.InputFromConsole();
+        var actual = sut.InputFromConsole()
+            .TryGetValue(x => throw new ApplicationException());
 
         var values = actual.deconstruct();
         values.Item1.Should().Be(surName);
@@ -31,17 +32,46 @@ public class QuestionnaireServiceTests
 
     [Theory]
     [AutoMoqData]
+    public void TestInputFromConsole_WhenError(Mock<IConsole> mock)
+    {
+        mock.Setup(x => x.ReadLine())
+            .Returns(() => throw new IOException());
+        ConsoleHelper.console = mock.Object;
+        var sut = new QuestionnaireService();
+
+        var actual = sut.InputFromConsole();
+
+        actual.Should().BeOfType<None<QuestionnaireBase>>();
+    }
+
+    [Theory]
+    [AutoMoqData]
     public void TestPrintToConsole(Mock<IConsole> mock, QuestionnaireBase questionnaire)
     {
         var expecteds = questionnaire.deconstruct();
         ConsoleHelper.console = mock.Object;
         var sut = new QuestionnaireService();
 
-        sut.PrintToConsole(questionnaire);
-
+        var actual = sut.PrintToConsole(questionnaire);
+        
+        actual.Should().BeOfType<Some>();
         mock.Verify(x => x.WriteLine("Склеивание:"));
         mock.Verify(x => x.WriteLine("Форматирование:"));
         mock.Verify(x => x.WriteLine("Интерполяция:"));
         mock.Verify(x => x.WriteLine($"{expecteds.Item1} {expecteds.Item2} {expecteds.Item3} лет {expecteds.Item4} см {expecteds.Item5} кг"));
+    }
+
+    [Theory]
+    [AutoMoqData]
+    public void TestPrintToConsole_WhenError(Mock<IConsole> mock, QuestionnaireBase questionnaire)
+    {
+        mock.Setup(x => x.WriteLine(It.IsAny<string>()))
+            .Throws(new IOException());
+        ConsoleHelper.console = mock.Object;
+        var sut = new QuestionnaireService();
+
+        var actual = sut.PrintToConsole(questionnaire);
+
+        actual.Should().BeOfType<None>();
     }
 }
